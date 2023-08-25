b16d6a1efa558b24a18429058322c419c8f810a3
pinegin pavel <pavel.pinegin.essen@gmail.com>
2023-08-25 23:46:20 +0200

    null

--- main.cpp
+++ main.cpp
@@ -10,11 +10,10 @@ void example(int A) {
 
 int main()
 {
-    std::function<void()> A;
-    if (A) {
-        std::cout << "OK" << std::endl;
-    } else {
-        std::cout << "Not ok" << std::endl;
-    }
+    std::shared_future<int> A = std::async(std::launch::async, [](){return 10;}).share();
+    std::future<int> B = std::async(std::launch::async, [&A](){return A.get() + 1;});
+
+    std::cout << "A:" << A.get() << std::endl;
+    std::cout << "B:" << B.get() << std::endl;
     return 0;
 }
