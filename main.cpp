fa4b080da507c36c71f38759472763cc62a56bb9
pinegin pavel <pavel.pinegin.essen@gmail.com>
2023-09-20 00:53:45 +0200

    json_boost

--- main.cpp
+++ main.cpp
@@ -6,38 +6,47 @@
 #include "Qsort_Pool/threadpool.hpp"
 // #include <mylib.hpp>
 #include "new_lib/new_lib.hpp"
+int get_int()
+{
+    return 1;
+}
 
 int main()
 {
-    boost::json::object O;                      // пустой объект
-    // сария вызывов для вставки
-    O["id"] = 1;                                // insert a int
-    O[ "pi" ] = 3.141;                          // insert a double
-    O[ "happy" ] = true;                        // insert a bool
-    O[ "name" ] = "Boost";                      // insert a string
+    // boost::json::object O;                      // пустой объект
+    // // сария вызывов для вставки
+    // O[get()] = get_int();                                // insert a int
+    // O[ "pi" ] = 3.141;                          // insert a double
+    // O[ "happy" ] = true;                        // insert a bool
+    // O[ "name" ] = "Boost";                      // insert a string
     // std::cout << O << std::endl;
     /*Вместо того, чтобы создавать документ JSON с помощью серии вызовов функций, 
     можно создать его в одном операторе, используя список инициализаторов:*/
 
-    boost::json::value value = {
-                                {"id", 2},
-                                {"pi", 3.141},
-                                {"happy", true},
-                                {"name", "Boost"}
-                                };
-    boost::json::object A = {{"id", 2}};
-    std::cout << value << std::endl;
-    // std::cout << A << std::endl;
-    boost::json::value C = boost::json::parse("[1, 2, 3]");
-    // std::cout << C << std::endl;
-    std::string serial = boost::json::serialize(value);
-    // std::cout << serial << std::endl;
-    boost::json::serializer sr;
-    sr.reset(&value);
-    do
-    {
-        char buf[20];
-        std::cout << sr.read(buf) << std::endl;
-    } while (! sr.done());
+    // boost::json::value value = {
+    //                             {"id", 2},
+    //                             {"pi", 3.141},
+    //                             {"happy", true},
+    //                             {"name", "Boost"}
+    //                             };
+    // boost::json::object A = {{"id", 2}};
+    // std::cout << value << std::endl;
+    // // std::cout << A << std::endl;
+    // boost::json::value C = boost::json::parse("[1, 2, 3]");
+    // // std::cout << C << std::endl;
+    // std::string serial = boost::json::serialize(value);
+    // // std::cout << serial << std::endl;
+    // boost::json::serializer sr;
+    // sr.reset(&value);
+    // do
+    // {
+    //     char buf[20];
+    //     std::cout << sr.read(buf) << std::endl;
+
+    // } while (! sr.done());
+    // boost::json::value F = boost::json::parse(serial);
+    // test_json();
+    // std::cout << O << std::endl;
+    test_json();
     return 0;
 }
--- new_lib/new_lib.hpp
+++ new_lib/new_lib.hpp
@@ -1,4 +1,58 @@
 #pragma once
 #include <boost/json.hpp>
+#include <iostream>
 
-void d();
+// void d();
+
+// class Interface
+// {
+// public:
+//     virtual void print() = 0;
+//     void F()
+//     {
+//         std::cout << "F" << std::endl;
+//     }
+// };
+
+// inline void Interface::print()
+// {
+//     std::cout << "Interface" << std::endl;
+// }
+
+// class A: public Interface
+// {
+// public:
+//     void print() override
+//     {
+//         std::cout << "Child" << std::endl;
+//         // Interface::print();
+//     }
+
+//     void d()
+//     {
+//         std::cout << "print: d" << std::endl;
+//     }
+// };
+inline std::string get()
+{
+    std::string line;
+    std::cout << "get" << '\n';
+    std::getline(std::cin, line);
+    return line;
+}
+
+inline void test_json()
+{
+    // boost::json::value json {
+    //                         {"key", 2},
+    //                         {"id", 4},
+    //                         {"str", "string"}
+    //                         };
+    boost::json::object O {
+                        {"id", 1},
+                        {"key", "string"}
+                        };
+    auto it = O.find("id");
+    std::cout << it->value() << std::endl;
+    std::cout << O.find("key")->value() << std::endl;
+}
